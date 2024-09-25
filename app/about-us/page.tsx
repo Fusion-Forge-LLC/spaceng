ea72bf862850cfeb0110f91970ed32e98c4b92a4
Olumide Bambe <loyalsfc@gmail.com>
2024-09-25 18:28:01 +0100

    adjusted UI designs

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,19 +1,12 @@
-import {Phone} from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 
 import Footer from "@/components/footer/footer";
 import Header from "@/components/header/header";
 import Title from "@/components/main/title/title";
-import {DropdDown} from "@/components/style-guide/style-guide";
-import {Input} from "@/components/ui/input";
 import Wrapper from "@/components/wrapper/wrapper";
-import CommunityCard from "@/components/about-us/community/community-card";
 import MissionCards from "@/components/about-us/mission-cards/mission-cards";
 import MembersCard from "@/components/about-us/members-card/members-card";
-
-import contactUs from "../../public/contact-us.png";
+import Faq from "@/components/services/faq/faq";
 
 const members = [
   {
@@ -55,7 +48,7 @@ function Page() {
     <div>
       <Header />
       <main>
-        <section>
+        {/* <section>
           <Wrapper className="pt-10">
             <Title title="Get Started With SpaceNG" />
             <form action="">
@@ -159,9 +152,9 @@ function Page() {
               </button>
             </form>
           </Wrapper>
-        </section>
+        </section> */}
 
-        <section>
+        {/* <section>
           <Wrapper className="py-10">
             <Image alt="Image of a sitting room" src={contactUs} />
 
@@ -199,26 +192,50 @@ function Page() {
               />
             </ul>
           </Wrapper>
-        </section>
+        </section> */}
+
+        <article>
+          <Wrapper className="pb-28 pt-20">
+            <h3 className="text-lg font-medium mb-14">About SpacesNG Technologies Limited</h3>
+            <p className="leading-[2.3]">
+              At SpacesNG, we are revolutionizing the way businesses and professionals discover and
+              utilize workspaces and shortlets in Nigeria. Our innovative platform connects
+              entrepreneurs, startups, and established companies with a network of flexible and
+              affordable workspaces, empowering them to focus on growth and success. Our mission is
+              to bridge the gap between businesses and suitable workspaces, reducing the stress and
+              complexity associated with finding the perfect location. By providing a user-friendly
+              platform for hosts to showcase their properties and for users to easily find and book
+              spaces, we aim to foster a thriving community of innovators and entrepreneurs. Founded
+              by a team of seven visionary individuals passionate about solving real-world problems,
+              SpacesNG is committed to delivering exceptional service, flexibility, and value to our
+              users. We believe that by working together, we can create a more productive,
+              connected, and successful business ecosystem.
+              <br />
+              <br />
+              Join us in shaping the future of workspaces and shortlets in Nigeria. List your
+              property, find your ideal space, and experience the SpacesNG difference.
+            </p>
+          </Wrapper>
+        </article>
 
         <section className="shadow-[0px_0px_25px_rgba(0,0,0,0.25)] py-16">
           <Wrapper className="py-10">
             <ul className="grid grid-cols-3 gap-16">
               <MissionCards title="Mission">
-                <p className="mt-auto">
+                <p className="mt-auto text-sm leading-relaxed mb-auto">
                   Empower individuals, teams, and organizations to thrive in an ever-evolving world
                   by forging innovative solutions, spaces, and experiences that meet their changing
                   needs
                 </p>
               </MissionCards>
               <MissionCards title="Vision">
-                <p className="mt-auto">
+                <p className="mt-auto text-sm leading-relaxed mb-auto">
                   To become the leading innovation hub, renowned for harnessing technology,
                   creativity, and community to create a better future for all
                 </p>
               </MissionCards>
               <MissionCards title="Value">
-                <ul className="mt-auto">
+                <ul className="mt-auto text-sm leading-relaxed">
                   <li className="values-list">Innovate Fearlessly</li>
                   <li className="values-list">User-centric</li>
                   <li className="values-list">Collaborate Openly</li>
@@ -238,6 +255,15 @@ function Page() {
                 return <MembersCard key={index} name={item.name} role={item.role} />;
               })}
             </ul>
+          </Wrapper>
+        </section>
+
+        <section>
+          <Wrapper className="py-10">
+            <h3 className="font-medium text-4xl mb-10 text-center">
+              Frequently Asked Questions (FAQs)
+            </h3>
+            <Faq />
           </Wrapper>
         </section>
       </main>
--- components/services/card/service-cards.tsx
+++ components/services/card/service-cards.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Servicecards({note, title, Icon}: {note: string; title: string; Icon: React.FC}) {
   return (
     <li className="border border-grey-100 p-3 space-y-3">
-      <span className="grid place-content-center h-14 w-14 bg-blue rounded-full">
+      <span className="grid place-content-center h-12 w-12 bg-grey rounded-full">
         <Icon />
       </span>
 
